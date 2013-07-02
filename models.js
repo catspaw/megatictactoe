08384665c9c0655414e1366d7c03fe840f2bf469
Michelle Levesque <catspaw@gmail.com>
2013-07-01 17:17:34 -0700

    Add a medium AI opponent

--- models.js
+++ models.js
@@ -77,7 +77,6 @@
     refresh: function(square) {
       if (!(this.isWon()) && this.checkForWin()) {
         this.set('winnerToken', square.get('token'));
-        console.log("New winner!");
       }
       this.set('lastClickIndex', this.get('squares').indexOf(square));
     },
@@ -128,11 +127,11 @@
         boards: new models.Boards(),
         currentToken: "X",  // X gets to go first.
         opponent: "player",
-        ai: new models.Opponent({game: this})
       }
     },
   
     initialize: function() {
+      this.set("ai", new models.Opponent({game: this}));
       this.get('boards').on('change:winnerToken', this.checkEndState, this);
       this.get('boards').on('change:lastClickIndex', this.prepForNextMove, this);
     },
@@ -148,7 +147,10 @@
         this.set("currentToken", "O");
         if (this.get("opponent") != "player") {
           // Have our AI make the next move.
-          this.get("ai").move();
+          var game = this;
+          window.setTimeout( function() {
+            game.get("ai").move();
+          }, 1);
         }
       } else {
         this.set("currentToken", "X");
@@ -208,15 +210,109 @@
 
     initialize: function() {
       this.get("game").on('change:opponent', this.changeDifficulty, this);
+      this.get("game").get("boards").on('change:lastClickIndex',
+                                        this.updateGameState, this);
+                                        
+      this.one_away_from_x_win = {0: false, 1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false};
+      this.one_away_from_o_win = {0: false, 1: false, 2: false, 3: false, 4: false, 5: false, 6: false, 7: false, 8: false};
     },
     
-    move: function() {
+    move: function() {      
+      if (this.get("difficulty") == "easy") {
+        this.easy_move();
+      } else if (this.get("difficulty") == "medium") {
+        this.medium_move();
+      }
+    },
+    
+    // Select a move at random from the list of available moves.
+    easy_move: function() {
       var board = this.get("game").getCurrentBoard();
       var choices = board.available_squares();
       var randomIndex = Math.floor(Math.random()*choices.length);
       
-      console.log("I choose square " + choices[randomIndex]);
+      console.log("I randomly choose square " + choices[randomIndex]);
       board.get("squares").at(choices[randomIndex]).place(this.get("token"));
+    },
+    
+    // For each board we store:
+    //   * Is the human player one-move-away from winning the board?
+    //   * Am I one-move-away from winning the board?  (And if so, what is it?)
+    updateGameState: function(board, clickIndex) {
+      var board_index = board.collection.indexOf(board);
+      var choices = board.available_squares();
+
+      this.one_away_from_o_win[board_index] = false;
+      this.one_away_from_x_win[board_index] = false;
+      
+      if (board.isWon()) {     
+        return;  // This board has already been won.
+      }
+      
+      for (var i=0; i<choices.length; i++) {
+        var o_move = this.simulate_board_move(board, choices[i], "O");
+        if (o_move.isWon()) {
+          this.one_away_from_o_win[board_index] = choices[i];
+        }
+        var x_move = this.simulate_board_move(board, choices[i], "X");
+        if (x_move.isWon()) {
+          this.one_away_from_x_win[board_index] = choices[i];
+        }
+      }
+      
+    },
+    
+    // medium_move uses the following algorithm:
+    //    If any of the moves allow me to win immediately, take it.
+    //    If any of the moves block an opponent win, take it.
+    //   Otherwise, randomly pick a spot on the board.
+    //
+    medium_move: function() {
+      var board = this.get("game").getCurrentBoard();
+      var board_index = board.collection.indexOf(board);
+      var choices = board.available_squares();
+      
+      var instant_win = this.one_away_from_o_win[board_index];
+      if (instant_win != false) {
+        console.log("Moved to " + instant_win + " to win the board.");
+        return board.get("squares").at(instant_win).place(this.get("token"));
+      }
+      
+      var block_lose = this.one_away_from_x_win[board_index];
+      if (block_lose != false) {
+        console.log("Moved to " + block_lose + " to block a row.");
+        return board.get("squares").at(block_lose).place(this.get("token"));
+      }
+      
+      this.easy_move();
+    },
+ 
+    // hard_move uses the following algorithm:
+    //    If any of the moves allow me to win immediately, take it.
+    //    If any of the moves block an opponent win, take it.
+    //    If the move results in a jump to a board where the opposite player
+    //      would win the board, skip that move.  (If possible.)
+    //    If the move results in a jump to a board where the opposite player
+    //      blocks a 3-in-a-row we would otherwise have, skip that move.
+    //      (If possible.)
+    //   Otherwise, randomly pick a spot on the board.
+    //
+    hard_move: function() {
+    },
+    
+    // Returns a new Board object, cloning the original board but placing
+    // the given token at the given move location.
+    simulate_board_move: function(board, move, token) {
+      // Clone the given board
+      var squares = board.get("squares");
+      var clone = new models.Board();
+      clone.fill();
+      for (var j=0; j<9; j++) {
+        clone.get("squares").at(j).set("token", squares.at(j).get("token"));
+      }
+      // Make the new move
+      clone.get("squares").at(move).place(token);
+      return clone;
     },
     
     changeDifficulty: function() {
